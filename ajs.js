28a8c77929fd01c3c5d5f383143e060094bda1f0
luisSurec <luissurec74@gmail.com>
2022-12-11 00:33:23 -0600

    primera carga js

---
+++ ajs.js
@@ -0,0 +1,21 @@
+let numeros =[21,45,100,12,11,78,61,4,39,22];
+
+for(let numero of numeros){
+Process.stdout.write(numeros +"");
+
+
+}
+
+
+
+let numeros =[21,45,100,12,11,78,61,4,39,22];
+
+for(let numero of numeros){
+console.log(numero +"");
+
+}
+for(let numero of numeros){
+    if(numeros/2 ==0){ console.log(numero);}
+
+
+}
